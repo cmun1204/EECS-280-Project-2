e918359667ceb4a0657357273b7d5c2e861b8426
Jacob Harleton <harleton@umich.edu>
2026-02-04 15:15:18 -0500

    added resize stuff

--- resize.cpp
+++ resize.cpp
@@ -29,7 +29,8 @@ int main(int argc, char* argv[]) {
     if (argc == 5) new_height = atoi(argv[4]);
     
 
-    if (new_width <= 0 || new_width > Image_width(&img) || new_height <= 0 || new_height > Image_height(&img)) {
+    if (new_width <= 0 || new_width > Image_width(&img) || new_height <= 0
+     || new_height > Image_height(&img)) {
     cout << "Usage: resize.exe IN_FILENAME OUT_FILENAME WIDTH [HEIGHT]\n"
      << "WIDTH and HEIGHT must be less than or equal to original" << endl;
      return 1;
